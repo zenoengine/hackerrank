c69b7438d528bd34e24c34c48afc27836f027147
zenoengine <zenoengine@gmail.com>
2019-12-31 01:49:47 +0900

    solve count-primes

---
+++ leetcode/count-primes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> v;
+        v.resize(n+1);
+        v[0] = true;
+        v[1] = true;
+
+        for(int i = 2; i < n; i++) {
+            for(int j = 2; j*i < n; j++) {
+                v[i*j] = true;
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n; i++){
+            if (v[i] == false){
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
