dcfac41061e476e174f3091003094849436718ae
Zeno Ahn <zenoengine@gmail.com>
2020-01-05 02:35:16 +0900

    solve find-all-numbers-disappeared-in-an-array

---
+++ leetcode/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        unordered_set<int> s;
+        for(int i = 0; i < nums.size(); i++){
+            s.insert(nums[i]);
+        }
+        for(int i = 1; i <= nums.size(); i++){
+            if (s.find(i) == s.end()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
