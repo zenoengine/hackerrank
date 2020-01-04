b2aa85e83b8d454dd3dda90641cb04ad4f3eea20
zenoengine <zenoengine@gmail.com>
2020-01-04 22:29:35 +0900

    optimize rotate-array

--- leetcode/rotate-array.cpp
+++ leetcode/rotate-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    void reverse(vector<int>& nums, int begin, int end) {
+        int j = 0;
+        for(int i = begin ; i < (end+begin)/2; i++) {
+            swap(nums[i], nums[end-1-j]);
+            j++;
+        }
+    }
+
     void rotate(vector<int>& nums, int k) {
         int shift = k % nums.size();
         if(shift == 0){
             return;
         }
 
-        vector<int> cp;
-        cp.reserve(nums.size());
-        for(int i = nums.size() - shift; i < nums.size(); i++){
-            cp.push_back(nums[i]);
-        }
-
-        for(int i = 0; i < nums.size() - shift; i++){
-            cp.push_back(nums[i]);
-        }
-        nums = cp;
+        reverse(nums, 0, nums.size());
+        reverse(nums, 0, shift);
+        reverse(nums, shift, nums.size());
     }
 };
